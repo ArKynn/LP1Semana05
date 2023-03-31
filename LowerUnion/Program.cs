10d8417f2830d7372abbd61524b336a561d63701
David Mendes <david.f.mendes@proton.me>
2023-03-31 18:23:42 +0100

    Added LowerUnion Project

---
+++ LowerUnion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace LowerUnion
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Array.Sort(args);
+            StringBuilder output = new StringBuilder();
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                output.Append((args[i].ToLower()));
+                if (i != args.Length - 1)
+                {
+                    output.Append('_');
+                }
+            }
+            Console.WriteLine($"Rearranged string: {output}");
+        }
+    }
+}
